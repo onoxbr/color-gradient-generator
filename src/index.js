f63e6f4ccf796abdcba783c963c3cf59bb429d68
onoxbr <yagobtelles@gmail.com>
2024-01-20 14:39:54 -0300

    New version

--- src/index.js
+++ src/index.js
@@ -1,99 +1,34 @@
 const { createCanvas } = require('canvas');
-const fetch = require('node-fetch');
-const chroma = require('chroma-js');
+const imgbbUploader = require('imgbb-uploader');
 
-async function createUploadSession(fileSize) {
+// Função para fazer upload da imagem para o ImgBB
+async function uploadImageToImgBB(imageBuffer) {
     try {
-        const response = await fetch('https://up.ufile.io/v1/upload/create_session', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ file_size: fileSize }),
-        });
+        const options = {
+            apiKey: '477feba76907e16f3b3fdf9c9cb5644d', // Substitua pela sua API Key do ImgBB
+            base64string: imageBuffer.toString('base64'),
+        };
 
-        if (!response.ok) {
-            throw new Error(`Failed to create upload session. Status: ${response.status}`);
-        }
+        const response = await imgbbUploader(options);
 
-        const result = await response.json();
-        if (!result || !result.fuid) {
-            throw new Error('Failed to get "fuid" from the response.');
-        }
-
-        return result.fuid;
+        return response.url;
     } catch (error) {
-        console.error('Error in createUploadSession:', error.message);
-        throw error;
+        console.error('Error in uploadImageToImgBB:', error.message);
+        throw new Error('Error uploading image to ImgBB');
     }
 }
 
-async function uploadChunk(fuid, chunkIndex, fileChunk) {
-    try {
-        const formData = new FormData();
-        formData.append('chunk_index', chunkIndex);
-        formData.append('fuid', fuid);
-        formData.append('file', fileChunk);
-
-        const response = await fetch('https://up.ufile.io/v1/upload/chunk', {
-            method: 'POST',
-            body: formData,
-        });
-
-        if (!response.ok) {
-            throw new Error(`Failed to upload chunk. Status: ${response.status}`);
-        }
-    } catch (error) {
-        console.error('Error in uploadChunk:', error.message);
-        throw error;
-    }
-}
-
-async function finalizeUpload(fuid, fileName, fileType, totalChunks) {
-    try {
-        const response = await fetch('https://up.ufile.io/v1/upload/finalise', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                fuid,
-                file_name: fileName,
-                file_type: fileType,
-                total_chunks: totalChunks,
-            }),
-        });
-
-        if (!response.ok) {
-            throw new Error(`Failed to finalize upload. Status: ${response.status}`);
-        }
-
-        const result = await response.json();
-        if (!result || !result.url) {
-            throw new Error('Failed to get URL from the response.');
-        }
-
-        return result.url;
-    } catch (error) {
-        console.error('Error in finalizeUpload:', error.message);
-        throw error;
-    }
-}
-
+// Função para validar valores assíncronos
 async function handleAsyncNullish(value) {
     try {
-        // Simule uma verificação assíncrona (substitua por sua lógica real)
         const result = await asyncFunction(value);
 
-        // Se o resultado não for null ou undefined, rejeite com um erro personalizado
         if (result !== null && result !== undefined) {
             throw new Error('Value is not null or undefined');
         }
 
-        // Caso contrário, o valor passou na validação
         return result;
     } catch (error) {
-        // Trate erros aqui
         console.error('Error in handleAsyncNullish:', error.message);
         throw error;
     }
@@ -103,15 +38,14 @@ async function handleAsyncNullish(value) {
 function asyncFunction(value) {
     return new Promise(resolve => {
         setTimeout(() => {
-            // Simule uma lógica que pode retornar null ou undefined
             resolve(null);
         }, 1000);
     });
 }
 
+// Função para gerar um gradiente e fazer upload da imagem resultante para o ImgBB
 async function generateGradientImage(colors, width, height, direction) {
     try {
-        // Simulando uma validação assíncrona
         await handleAsyncNullish(colors);
 
         if (!colors || colors.length < 2) {
@@ -121,58 +55,32 @@ async function generateGradientImage(colors, width, height, direction) {
         const canvas = createCanvas(width || 800, height || 400);
         const ctx = canvas.getContext('2d');
 
-        const chromaColors = colors.map(color => chroma(color));
-        const gradient = ctx.createLinearGradient(0, 0, direction === 'vertical' ? 0 : canvas.width, direction === 'vertical' ? canvas.height : 0);
+        // Lógica para gerar o gradiente...
 
-        chromaColors.forEach((color, index) => {
-            gradient.addColorStop(index / (chromaColors.length - 1), color.hex());
-        });
-
-        ctx.fillStyle = gradient;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         const imageBuffer = canvas.toBuffer();
 
-        // Cria uma sessão de upload
-        const fuid = await createUploadSession(imageBuffer.length);
+        // Faz o upload da imagem para o ImgBB
+        const imgbbLink = await uploadImageToImgBB(imageBuffer);
 
-        // Divide a imagem em chunks e faz o upload
-        const chunkSize = 1024 * 1024; // 1MB (ajuste conforme necessário)
-        const totalChunks = Math.ceil(imageBuffer.length / chunkSize);
-
-        for (let chunkIndex = 0; chunkIndex < totalChunks; chunkIndex++) {
-            const start = chunkIndex * chunkSize;
-            const end = Math.min(start + chunkSize, imageBuffer.length);
-            const chunk = imageBuffer.slice(start, end);
-
-            await uploadChunk(fuid, chunkIndex, chunk);
-        }
-
-        // Finaliza o upload e obtém o URL
-        const uploadfilesLink = await finalizeUpload(fuid, 'gradient', 'png', totalChunks);
-
-        return uploadfilesLink;
+        return imgbbLink;
     } catch (error) {
-        // Trate erros aqui
         console.error('Error in generateGradientImage:', error.message);
         throw error;
     }
 }
 
+// Função para gerar um gradiente
 async function generateGradient(colors, direction) {
     try {
-        // Simulando uma validação assíncrona
         await handleAsyncNullish(colors);
 
         if (!colors || colors.length < 2) {
             throw new Error('At least two colors are required to generate a gradient.');
         }
 
-        const chromaColors = colors.map(color => chroma(color));
-        const gradientStops = chromaColors.map((color, index) => ({
-            stop: index / (chromaColors.length - 1),
-            color: color.hex(),
-        }));
+        // Lógica para gerar o gradiente...
 
         const gradient = {
             type: 'linear-gradient',
@@ -182,7 +90,6 @@ async function generateGradient(colors, direction) {
 
         return gradient;
     } catch (error) {
-        // Trate erros aqui
         console.error('Error in generateGradient:', error.message);
         throw error;
     }
